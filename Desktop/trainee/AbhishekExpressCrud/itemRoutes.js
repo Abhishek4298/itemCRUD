9cb8e451593d6a9a1b3d627d6b332f8418ac25be
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-30 16:30:10 +0530

    rest api in endpoint

--- Desktop/trainee/AbhishekExpressCrud/itemRoutes.js
+++ Desktop/trainee/AbhishekExpressCrud/itemRoutes.js
@@ -2,73 +2,57 @@ const express = require('express');
 const itemModel = require('./itemModel');
 const app = express();
 
-//1.create item
-app.post('/create', async (req, res) => {
+// 1.create item
+app.post('/item/create', async (req, res) => {
   const createItem = new itemModel(req.body);
-  try
-  {
+  try {
    createItem.save();
     res.send("Added succesfully");
-  }
-  catch(err)
-  {
+  } catch(err) {
     console.log("error")
   }
 });
-//2. item price 
-app.post('/addprice', async (req, res) => {
+
+// 2. item price 
+app.put('/item/addPrice/:id', async (reqs, res) => {
  
-  try
-  {
+  try{
     const updatedData = itemModel.findOneAndUpdate({
-      _id: req.body.id
+        id: req.params.id
     },{
-      price: 5000
+        $set:{price: req.body.price}
     },{
       new:true,
     });
-    console.log("updatedData price", updatedData)
-   
+    console.log("updatedData price", updatedData) 
     res.send(" price Added succesfully");
-  }
-  catch(err)
-  {
+  } catch(err) {
     console.log("error")
   }
 });
 
-
-
-
-
-//4.find item
-app.get('/search', async (req, res) => {
+// 4.find item
+app.get('/item/search', async (req, res) => {
   const searchItem = await itemModel.find({});
-  try
-  {
+  try{
     res.send(searchItem);
     console.log("getting all the items")
-
-  }
-  catch(err)
-  {
+  } catch(err) {
     console.log("error")
   }
 });
 
-//5.Discription of a specific item by id
-app.get('/getById', async (req, res) => {
+// 5.Discription of a specific item by id
+app.get('/item/getById', async (req, res) => {
   const specItemDesc = await itemModel.find({id: +Object.values(req.body)[0]},{description:1});
-  try
-  {
+  
+  try{
     res.send(specItemDesc);
-    console.log("Description of a specific item ")
-    console.log(specItemDesc)
-
-  }
-  catch(err)
-  {
+    console.log ("Description of a specific item ");
+    console.log (specItemDesc);
+  } catch(err) {
     console.log("error")
   }
 });
+
 module.exports = app
